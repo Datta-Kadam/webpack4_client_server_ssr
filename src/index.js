99b0944d7b8c7974e02a0505d89be8571ab2063b
Datta Kadam <datta.k1985@gmail.com>
2018-04-13 11:08:26 -0500

    Universal routes added under ruotes.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 import routes from './routes';
 import reducers from './reducers';
 
--- src/routes.1.js
+++ src/routes.1.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import universal from 'react-universal-component';
 import Simulate_form from '../src/components/simulate_form';
 import Simulate_form_Update from '../src/components/simulate_form_update';
 import Headers from '../src/components/header';
 import RequireAuth from './components/auth/req_auth';
 
 
-const UniversalComponent = universal(props => import(`./${props.page}`));
-
 const routes = (  
     <div>
       <Headers />
        <Switch>
         <Redirect from="/" exact to="/AddRequestResponse" />
-         <Route exact path="/AddRequestResponse">
-            <UniversalComponent page="Simulate_form" />
-         </Route>       
-         <Route path="/UpdateRequestResponse">
-            <UniversalComponent page="Simulate_form_Update" />
-         </Route>   
+         <Route exact path="/AddRequestResponse" component={RequireAuth(Simulate_form)} />       
+         <Route path="/UpdateRequestResponse" component={RequireAuth(Simulate_form_Update)} />
        </Switch>
     </div>
 );
